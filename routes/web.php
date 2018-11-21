35df17d513c6810bf35ab33648f93eebcba36c6c
mason <793789487@qq.com>
2018-11-21 05:56:56 +0000

    完成登录

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,11 @@ Route::get('/', 'PagesController@index')->name('index');
 
 Route::get('signup', 'UsersController@create')->name('signup');
 
+// 用户资源
 Route::resource('users', 'UsersController');
+
+// 登录
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
+
